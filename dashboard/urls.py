ef391f172e5f5ffc1454e68e00928bae016aebf1
Shivam Kumar Mishra <shivammishra2408@gmail.com>
2021-07-26 22:05:15 +0530

    books added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -18,5 +18,4 @@ urlpatterns = [
     path('dictionary',views.dictionary,name='dictionary'),
     path('wiki',views.wiki,name='wiki'),
     path('conversion',views.conversion,name="conversion"),
-
 ]
